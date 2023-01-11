90e4e9e72b6136e484b0c3c39fd2ba886a9be341
Arnaud293 <arnaudlt293@gmail.com>
2023-01-11 16:31:14 +0100

    add navlinks as objects

---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const logo = [
+    {
+      id: 1,
+      imgUrl: '/logoNit.png',
+    },
+    {
+      id: 2,
+      imgUrl: '/logoMin.png',
+    },
+    
+];
+
+export const navLinks = [
+    {
+        id: 1,
+        link : 'Home'
+    },
+    {
+        id: 2,
+        link : 'Movies'
+    },
+    {
+        id: 3,
+        link : 'Tv Shows'
+    },
+    {
+        id: 4,
+        link : 'Favorites'
+    },
+];
