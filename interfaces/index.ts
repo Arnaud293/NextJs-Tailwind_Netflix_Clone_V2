493ca17b53a3540400f980a5063fab6090624bd4
Arnaud293 <arnaudlt293@gmail.com>
2023-01-12 15:39:38 +0100

    create interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,17 @@
+export interface TrendingsData {
+    poster_path: string | null,
+    adult: boolean,
+    overview: string,
+    release_date: string,
+    genres_ids: Array<number>,
+    id: number,
+    original_title: string,
+    original_language: string,
+    title: string,
+    backdrop_path: string | null,
+    popularity: number,
+    vote_count: number,
+    video: boolean,
+    vote_average: number
+
+}
