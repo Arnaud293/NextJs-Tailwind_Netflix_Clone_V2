546e6eec276d32f3191e64889b1b08ebcec47982
Arnaud293 <arnaudlt293@gmail.com>
2023-01-13 13:02:39 +0100

    fetch and render banner img

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-type Props = {}
+import requests from '../constants/requests';
 
-const Banner:React.FC = ({}: Props) => {
+import { TrendingsDataMovies, TrendingsDataTv } from '../interfaces';
+
+const Banner:React.FC = () => {
+
+  const [trending, setTrending] = useState<TrendingsDataMovies | TrendingsDataTv | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const req  = await axios.get(requests.getTrending);
+      const data: (TrendingsDataMovies| TrendingsDataTv)[] = req.data.results;
+      setTrending(
+        data.filter(el => el.poster_path !== null)[
+          Math.floor(Math.random() * data.length - 1)
+        ]
+      )
+      console.log(data);
+      
+    }
+    getData()
+  },[]);
+
   return (
-    <div>Banner</div>
+    <div className='w-full relative h-screen'>
+      <img src={`https://image.tmdb.org/t/p/original/${trending?.poster_path}`} alt="" className='w-full h-full object-cover'/>
+    </div>
   )
 }
 
