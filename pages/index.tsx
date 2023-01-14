1dd12f91cba446669cb209de59d415ce17ea76fa
Arnaud293 <arnaudlt293@gmail.com>
2023-01-14 12:02:23 +0100

    fix style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Banner } from '../components'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
       <Head>
         <title>Nitflex-V2</title>
       </Head>
+      <Banner />
     </div>
   )
 }
