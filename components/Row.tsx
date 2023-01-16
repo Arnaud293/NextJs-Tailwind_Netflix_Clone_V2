2655bc85d211eec20277e1d2b8f512d0b8211f5a
Arnaud293 <arnaudlt293@gmail.com>
2023-01-16 15:04:02 +0100

    first map of movies/tv in their own category, slice to top 10, miss style, some type issues with array method

---
+++ components/Row.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import { TrendingsDataMovies, TrendingsDataTv } from '../interfaces';
+
+
+const Row = ({ fetchUrl, title }) => {
+
+  const [programs, setPrograms] = useState<TrendingsDataMovies[] | TrendingsDataTv[] | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const req  = await axios.get(fetchUrl);
+      const data: (TrendingsDataMovies| TrendingsDataTv)[] = req.data.results;
+      setPrograms(
+        data.filter(el => el.backdrop_path !== null)
+      )
+      console.log(data);
+      
+    }
+    getData()
+  },[]);
+
+
+  return (
+    <div className='p-4'>
+        <h2 className='text-white text-2xl font-semibold'>{title}</h2>
+        <div className="flex overflow-x-hidden gap-2 mt-2 ">
+            {programs?.slice(0,10).map((program, index) => (
+                <img src={`https://image.tmdb.org/t/p/original/${program?.backdrop_path}`} alt={program?.title | program?.name} className='w-[200px] h-[120px] rounded-sm' />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Row
