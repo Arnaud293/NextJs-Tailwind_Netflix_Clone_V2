7372c9c2640a46c42c2be67a731c4a99b1f044d7
Arnaud293 <arnaudlt293@gmail.com>
2023-01-16 15:13:11 +0100

    add scroll X to listed programs

--- components/Row.tsx
+++ components/Row.tsx
@@ -22,9 +22,9 @@ const Row = ({ fetchUrl, title }) => {
 
 
   return (
-    <div className='p-4'>
+    <div className='p-4 overflow-x-hidden'>
         <h2 className='text-white text-2xl font-semibold'>{title}</h2>
-        <div className="flex overflow-x-hidden gap-2 mt-2 ">
+        <div className=" w-full flex overflow-x-scroll gap-2 mt-2 ">
             {programs?.slice(0,10).map((program, index) => (
                 <img src={`https://image.tmdb.org/t/p/original/${program?.backdrop_path}`} alt={program?.title | program?.name} className='w-[200px] h-[120px] rounded-sm' />
             ))}
