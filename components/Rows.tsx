f2e4977247d183762f9ba207594af91e4193b38b
Arnaud293 <arnaudlt293@gmail.com>
2023-01-16 15:01:57 +0100

    map row over rows => more dynamic than V1

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { HomeRequests } from '../constants';
+import Row from './Row';
 
 type Props = {}
 
 const Rows:React.FC = ({}: Props) => {
+
   return (
-    <div>Rows</div>
+    <div>
+      {HomeRequests.map((row, index) => (
+        <Row key={index} fetchUrl={row.fetchUrl} title={row.title} />
+      ))}
+    </div>
   )
 }
 
