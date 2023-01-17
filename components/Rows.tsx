f056e8f8bcc0fc5f738c821bc92beed27a38d625
Arnaud293 <arnaudlt293@gmail.com>
2023-01-17 15:39:03 +0100

    add switch case to fetch and map data depending of the current path/page

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
-import { HomeRequests } from '../constants';
+import { HomeRequests, MoviesRequests, TvRequests } from '../constants';
 import Row from './Row';
+import { useRouter } from 'next/router';
+import { request } from '../interfaces';
 
-type Props = {}
+const Rows:React.FC = () => {
 
-const Rows:React.FC = ({}: Props) => {
+  const [query, setQuery] = useState<request[]>([]);
+  const router = useRouter();
+  console.log(router)
+
+  useEffect(() => {
+    const requestsToDo = router.asPath;
+    switch(requestsToDo){
+      case '/':
+        setQuery(HomeRequests);
+        break;
+      case '/movies':
+        setQuery(MoviesRequests);
+        break;
+      case '/tv':
+        setQuery(TvRequests);
+        break;
+      default: return null;
+    }
+  },[])
 
   return (
-    <div>
-      {HomeRequests.map((row, index) => (
+    <div className='bg-gray-900'>
+      {query.map((row, index) => (
         <Row key={index} fetchUrl={row.fetchUrl} title={row.title} />
       ))}
     </div>
