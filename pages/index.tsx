1646d81bbd6dd9a0a6d2f67debc98bc37c4cacd2
Arnaud293 <arnaudlt293@gmail.com>
2023-01-17 14:09:23 +0100

    create internal  endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Banner } from '../components'
+import { Banner, Rows } from '../components'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
+    <div className="flex min-h-screen flex-col bg-gray-900">
       <Head>
         <title>Nitflex-V2</title>
       </Head>
       <Banner />
+      <Rows />
     </div>
   )
 }
