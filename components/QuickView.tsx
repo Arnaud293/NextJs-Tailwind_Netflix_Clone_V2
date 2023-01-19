374c2004b7403f535fe53eb35fdde508e0d3cebc
Arnaud293 <arnaudlt293@gmail.com>
2023-01-19 12:35:32 +0100

    first render with img to test

--- components/QuickView.tsx
+++ components/QuickView.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { RxCross1 } from 'react-icons/rx';
 
-type Props = {}
 
-const QuickView:React.FC = ({}: Props) => {
+const QuickView:React.FC = ({ setDisplayQuickView, program}) => {
   return (
-    <div>QuickView</div>
+    <div className="fixed flex justify-center items-start backdrop-blur h-screen w-screen z-10 top-[100px] p-10">
+      <div className="w-[60vw] h-[60vh] bg-gray-900 rounded-md relative">
+        <RxCross1 size={30} onClick={() => setDisplayQuickView(false)} className='text-white absolute right-1 top-1'/>
+        <div className="w-full h-2/4">
+          <img src={`https://image.tmdb.org/t/p/original/${program?.backdrop_path}`} alt="" className='h-full w-full object-cover object-top' />
+        </div>
+      </div>
+  </div>
   )
 }
 
