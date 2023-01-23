4f2ec7b075b8f77d665980b5d33504a913ee65e9
Arnaud293 <arnaudlt293@gmail.com>
2023-01-23 18:55:06 +0100

    fix type error

--- components/Row.tsx
+++ components/Row.tsx
@@ -3,17 +3,18 @@ import React, {useEffect, useState} from 'react';
 import { TrendingsDataMovies, TrendingsDataTv, request } from '../interfaces';
 import QuickView from './QuickView';
 
+type UnionTrendingsData = TrendingsDataMovies | TrendingsDataTv;
 
 const Row:React.FC<request> = ({ fetchUrl, title }) => {
 
-  const [programs, setPrograms] = useState<TrendingsDataMovies[] | TrendingsDataTv[] | null>(null);
+  const [programs, setPrograms] = useState<UnionTrendingsData[] | null>(null);
   const [displayQuickView, setDisplayQuickView] = useState(false);
-  const [quickViewData, setQuickViewData] = useState([]);
+  const [quickViewData, setQuickViewData] = useState<UnionTrendingsData[]>([]);
 
   useEffect(() => {
     const getData = async () => {
       const req  = await axios.get(fetchUrl);
-      const data: TrendingsDataMovies[]| TrendingsDataTv[] = req.data.results;
+      const data: (TrendingsDataMovies| TrendingsDataTv)[] = req.data.results;
       setPrograms(
         data.filter(el => el.backdrop_path !== null)
       )
@@ -21,9 +22,11 @@ const Row:React.FC<request> = ({ fetchUrl, title }) => {
     getData()
   },[]);
 
-  const handleQuickView = (e) => {
+  const handleQuickView = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
     displayQuickView ? setDisplayQuickView(false) : setDisplayQuickView(true);
-    const data = programs?.filter(program => program.backdrop_path === e.target.src.split('https://image.tmdb.org/t/p/original/').join(""));
+    
+    const data: UnionTrendingsData[] = (programs || [])?.filter(program =>
+    program.backdrop_path === e.target.src.split('https://image.tmdb.org/t/p/original/').join(""));
     setQuickViewData(data);
   }
 
@@ -36,7 +39,7 @@ const Row:React.FC<request> = ({ fetchUrl, title }) => {
         <div className="w-full flex overflow-x-scroll gap-2 mt-2 py-2">
             {programs?.slice(0,10).map((program, index) => (
                 <img src={`https://image.tmdb.org/t/p/original/${program?.backdrop_path}`}
-                alt={program?.title | program?.name}
+                alt={program?.title || program?.name}
                 className='w-[200px] h-[120px] rounded-sm cursor-pointer hover:scale-105' 
                 onClick={(e) => handleQuickView(e)}/>
             ))}
