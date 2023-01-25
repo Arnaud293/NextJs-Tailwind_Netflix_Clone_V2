3081bf7b9376faba9dab98431e9b7ce56ce3527f
Arnaud293 <arnaudlt293@gmail.com>
2023-01-25 11:58:19 +0100

    get data passed into async function in useEffect

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -10,25 +10,30 @@ const Favorites:NextPage = ({}) => {
 
   // Get stored movies
   useEffect(() => {
+    const getFavoritesMovies = async () =>{
       let moviesArr:DetailsDataMovies[] = [];
       let moviesId = window.localStorage.movies ? window.localStorage.movies.split(',') : [];
       for (let i=0; i < moviesId.length; i++){
-          axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
+          await axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
           .then(res => moviesArr.push(res.data))
           .then(() => setStoredDataMovies(moviesArr));     
       }
+    }
+    getFavoritesMovies();
+
   },[])
   // Get stored Tv shows
   useEffect(() => {
+    const getFavoritesTv = async () => {
       let tvArr:DetailsDataTv[] = [];
       let tvId = window.localStorage.tv ? window.localStorage.tv.split(',') : [];
-      for (let i=0; i < tvId.length; i++){
-          axios.get(`https://api.themoviedb.org/3/tv/${tvId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
+      for (let i=0; i < tvId?.length; i++){
+         await axios.get(`https://api.themoviedb.org/3/tv/${tvId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
           .then(res => tvArr.push(res.data))
           .then(() => setStoredDataTv(tvArr));    
       }
-      console.log(setStoredDataTv);
-      
+    }
+    getFavoritesTv();
   },[])
   // Delete from favorites
   const handleDeleteFavoritesMovies = () => {
@@ -53,21 +58,37 @@ const Favorites:NextPage = ({}) => {
   }
 
   return (
-    <div className='h-screen bg-gray-900 text-white py-20'>
-      <div className="">
-        {storedDataMovies?.length 
+    <div className='h-screen bg-gray-900 text-white py-20 flex flex-col items-center'>
+      <h1>Favorites Tv Shows</h1>
+      <div className="flex">
+        {storedDataTv?.length 
         ? (
-          storedDataMovies.map((movie) => (
+          storedDataTv.map((show) => (
             <div>
-              <img src={movie.backdrop_path} alt={movie.title} />
+              <img src={`https://image.tmdb.org/t/p/original/${show.backdrop_path}`} alt={show.title}
+              className="w-[300px] h-[225px] object-cover" />
             </div>
           ))
         )
         :(
-          <h2>No favorites movies</h2>
+          <h2>No favorites tv shows</h2>
         )}
       </div>
-      <div className="">MOVIES</div>
+      <h1>Favorites Movies</h1>
+      <div className="flex">
+        {storedDataMovies?.length 
+          ? (
+            storedDataMovies.map((movie) => (
+              <div>
+                <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} 
+                className="w-[300px] h-[225px] object-cover"/>
+              </div>
+            ))
+          )
+          :(
+            <h2>No favorites movies</h2>
+          )}
+      </div>
     </div>
   )
 }
