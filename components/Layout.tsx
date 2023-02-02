bcaabd6f1dce9aeebeefb9e9db2cbdc8caa13c5f
Arnaud293 <arnaudlt293@gmail.com>
2023-02-02 16:51:06 +0100

    conditionnal rendering of children depending of research with the search bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'tailwindcss/tailwind.css';
-import { NavBar, Footer } from './';
+import { NavBar, Footer, SearchResult } from './';
 
 type ChildrenProps = {
     children?: React.ReactNode
 }
 
 const Layout:React.FC<ChildrenProps> = ({children}) => {
+  const [searchedResults, setSearchedResults] = useState('');
+  console.log(searchedResults);
   return (
     <>
-        <NavBar />
-        {children}
+        <NavBar result={searchedResults} setResult={setSearchedResults} />
+        {searchedResults.length > 0 
+        ? (
+            <SearchResult result={searchedResults} />
+          ) 
+        :(
+          children
+        )}
         <Footer />
     </>
   )
